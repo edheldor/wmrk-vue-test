b6b42ef6bafa05ef1aaa03c16bc9e3a4841f19e1
Edheldor <edheldor@gmail.com>
2020-02-19 20:00:48 +0300

    настройки для heroku

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+let history = require('connect-history-api-fallback')
+
+let express = require('express');
+let path = require('path');
+let serveStatic = require('serve-static');
 app = express();
+app.use(history());
 app.use(serveStatic(__dirname));
-var port = process.env.PORT || 5000;
+let port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started ' + port);
 
